ba0b268d091997875fce640b77af95f32a4ae0bb
Nick Campbell <nicholas.j.campbell@gmail.com>
2009-08-28 08:04:40 -0400

    Making use of API. If API keys are not specified, fallthrough to scraping site.

--- bitly.cpp
+++ bitly.cpp
@@ -44,10 +44,23 @@ QString Bitly::shorten( const QString &url )
 {
     kDebug()<<"Using bit.ly";
     QByteArray data;//output
-    KUrl reqUrl( "http://bit.ly/" );
-    reqUrl.addQueryItem( "s", "" );
-    reqUrl.addQueryItem( "keyword", "" );
-    reqUrl.addQueryItem( "url", KUrl( url ).url() );
+    KUrl reqUrl = null;
+    const QString apiname = KGlobal::config()->group("Advanced").readEntry("ShortenPlugin_APIName", QString());
+    const QString apikey = KGlobal::config()->group("Advanced").readEntry("ShortenPlugin_APIKey", QString());
+    kDebug() << "API Login: " << apiname;
+    kDebug() << "API Key: " << apikey;
+    if ( ! ( apiname.isEmpty() && apikey.isEmpty() ) ) {
+	reqUrl = new KUrl( "http://api.bit.ly/shorten" );
+	reqUrl.addQueryItem( "version", "2.0.1" );
+	reqUrl.addQueryItem( "longUrl", KUrl( url ).url() );
+	reqUrl.addQueryItem( "login", apiname );
+	reqUrl.addQueryItem( "apiKey", apikey );
+    } else {
+	reqUrl = new KUrl( "http://bit.ly/" );
+	reqUrl.addQueryItem( "s", "" );
+	reqUrl.addQueryItem( "keyword", "" );
+	reqUrl.addQueryItem( "url", KUrl( url ).url() );
+    }
 
     KIO::Job *job = KIO::get( reqUrl, KIO::Reload, KIO::HideProgressInfo );
 
